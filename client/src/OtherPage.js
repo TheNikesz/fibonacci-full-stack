b558b5f52bf56b99d0a663b2856795f3337287f8
TheNikesz <64257234+TheNikesz@users.noreply.github.com>
2023-01-21 17:37:24 +0100

    feat: add workflow

--- client/src/OtherPage.js
+++ client/src/OtherPage.js
@@ -4,7 +4,9 @@ import { Link } from 'react-router-dom';
 export default () => {
   return (
     <div>
-      Im some other page!
+      <p>btmultifib</p>
+      <p>Błażej Typek</p>
+      <p>I2S 1.5</p>
       <Link to="/">Go back home</Link>
     </div>
   );
